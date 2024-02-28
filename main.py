f62019229dcaea132ca9313a0562c398f502b915
Kağan Kaçar <110815289+KantoshK@users.noreply.github.com>
2024-02-28 16:56:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import time
     bot.run_discord_bot()"""
 
 #-----------------------------------------------
-    
-TOKEN = ''
 
-async def send_message(message, user_message, is_private):   
+TOKEN = 'ODE0MjM4MjY2NzkzNzg3Mzk0.GCz9-R.Diy8X7cRcOY6RLGnnNug_81tMirnJQMRW5j_EY'
+
+async def send_message(message, user_message, is_private):
     try:
         response = get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
@@ -22,11 +22,11 @@ async def send_message(message, user_message, is_private):
         return None
 
 def run_discord_bot():
-    TOKEN = ''
+    TOKEN = 'ODE0MjM4MjY2NzkzNzg3Mzk0.GCz9-R.Diy8X7cRcOY6RLGnnNug_81tMirnJQMRW5j_EY'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
-    
+
     @client.event
     async def on_ready():
         print(f"{client.user} su anda calisiyor")
@@ -35,11 +35,11 @@ def run_discord_bot():
     async def on_message(message):
         if message.author == client.user:
             return
-        
+
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
-        
+
         locale.setlocale(locale.LC_ALL, 'turkish')
         zaman = time.strftime('%c')
 
@@ -54,20 +54,20 @@ def run_discord_bot():
     client.run(TOKEN)
 
 #-----------------------------------------------------------
-    
+
 def get_response(message: str) -> str:
     meydan_list = []
     with open("meydan.txt", "r", encoding="UTF-8") as dosya:
         meydanlar = dosya.readlines()
         for meydan in meydanlar:
             meydan_list.append(meydan.replace("\n",""))
-        
+
         meydan_sec1 = random.choice(meydan_list)
         meydan_sec2 = random.choice(meydan_list)
 
         while meydan_sec1 == meydan_sec2:
             meydan_sec2 = random.choice(meydan_list)
-    
+
     prefix = "!"
     p_message = message.lower()
 
@@ -75,37 +75,42 @@ def get_response(message: str) -> str:
 
         if p_message == f'{prefix}komutlar':
             return 'Prefix: -- (Komutların başına -- yazılır)\n--rota_oner: İç hatlardan bir uçuş rotası önerir.'
-        
-        if p_message == f'{prefix}merhaba':
+
+        elif p_message == f'{prefix}merhaba':
             return 'Heyyy!'
-        
-        if p_message == f'{prefix}help':
+
+        elif p_message == f'{prefix}help':
             return 'Yardım geldi!'
         
-        if p_message == f'{prefix}rotaoner':
-            return f'{meydan_sec1}  >>>  {meydan_sec2}'
+        elif p_message == f'{prefix}deneme':
+            return 'deneme'
         
-        if p_message == f'{prefix}rotaoner-fm':
+        #deneme
+
+        elif p_message == f'{prefix}rotaoner':
+            return f'{meydan_sec1}  >>>  {meydan_sec2}'
+
+        elif p_message == f'{prefix}rotaoner-fm':
             while meydan_sec2 == meydan_list[0]:
                 meydan_sec2 = random.choice(meydan_list)
             return f'İstanbul Havalimanı (LTFM)  >>>  {meydan_sec2}'
-        
-        if p_message == f'{prefix}rotaoner-ac':
+
+        elif p_message == f'{prefix}rotaoner-ac':
             while meydan_sec2 == meydan_list[1]:
                 meydan_sec2 = random.choice(meydan_list)
             return f'Ankara Esenboğa Havalimanı (LTAC)  >>>  {meydan_sec2}'
-        
-        if p_message == f'{prefix}rotaoner-fj':
+
+        elif p_message == f'{prefix}rotaoner-fj':
             while meydan_sec2 == meydan_list[15]:
                 meydan_sec2 = random.choice(meydan_list)
             return f'İstanbul Sabiha Gökçen Havalimanı (LTFJ)  >>>  {meydan_sec2}'
-        
-        if p_message == f'{prefix}rotaoner-bj':
+
+        elif p_message == f'{prefix}rotaoner-bj':
             while meydan_sec2 == meydan_list[16]:
                 meydan_sec2 = random.choice(meydan_list)
             return f'İzmir Adnan Menderes Havalimanı (LTBJ)  >>>  {meydan_sec2}'
-        
+
         return 'Böyle bir komut yok'
-    
+
 if __name__ == '__main__':
     run_discord_bot()
